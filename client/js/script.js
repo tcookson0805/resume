488946026f781f7987a8ca49dbc5b9f5a3a0c28f
Tyler Cookson <tcookson0805@gmail.com>
2017-03-01 10:19:55 -0800

    updates

--- client/js/script.js
+++ client/js/script.js
@@ -1,59 +1,33 @@
 $(document).ready(function() {
+	
+	$('body').on('click', '.nav-link', function(event) {
 
-	$('body').scrollspy({ target: '#side_panel_links' })
+		if(this.hash !== ''){
 
+			event.preventDefault();
 
-	var header = $('.hero_headline').clone();
-	var about = $('.about_link_content').clone();
-	var skills = $('.skills_link_content').clone();
-	var projects = $('.projects_link_content').clone();
+			var hash = this.hash;
 
-	$('.hero_content').append(header)
+			$('body').animate({
+				scrollTop: $(hash).offset().top
+			}, 800, function() {
+				console.log('scrolled')
+				window.location.hash = hash;
+			})			
+		
+		} else {
+			
+			event.preventDefault();
 
-
-	var intro = $('.header_first').fadeIn('slow', function(){
-		$('.header_second').fadeIn('slow', function(){
-			$('.header_third').fadeIn('slow', function() {
-				$('.links').fadeIn('slow')
+			$('body').animate({
+				scrollTop: 0
+			}, 800, function() {
+				window.location.hash = '';
 			})
-		})
+
+		}
+
 	})
-
-
-	$('body').on('click', '.header', function() {
-		$('.about_link').removeClass('highlight');
-		$('.skills_link').removeClass('highlight');
-		$('.projects_link').removeClass('highlight');
-		$('.hero_content').children().remove();
-		$('.hero_content').append(header);
-	})
-
-	$('body').on('click', '.about_link', function() {
-		console.log('click')
-		$(this).toggleClass('highlight')
-		$('.skills_link').removeClass('highlight');
-		$('.projects_link').removeClass('highlight');
-		$('.hero_content').children().remove();
-		$('.hero_content').append(about);
-	});
-
-	$('body').on('click', '.skills_link', function() {
-		console.log('click')
-		$(this).toggleClass('highlight')
-		$('.about_link').removeClass('highlight');
-		$('.projects_link').removeClass('highlight');
-		$('.hero_content').children().remove();
-		$('.hero_content').append(skills);
-	});
-
-	$('body').on('click', '.projects_link', function() {
-		console.log('click')
-		$(this).toggleClass('highlight')
-		$('.about_link').removeClass('highlight');
-		$('.skills_link').removeClass('highlight');
-		$('.hero_content').children().remove();
-		$('.hero_content').append(projects);
-	});
 
 
 })
