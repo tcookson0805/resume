66bcb73996976703c5c53dace2386ec06c81037a
Tyler Cookson <tcookson0805@gmail.com>
2017-03-03 11:20:53 -0800

    finished form email

--- client/js/script.js
+++ client/js/script.js
@@ -1,32 +1,51 @@
 $(document).ready(function() {
 	
+	// scrolling
 	$('body').on('click', '.nav-link', function(event) {
-
 		if(this.hash !== ''){
-
 			event.preventDefault();
-
 			var hash = this.hash;
-
 			$('body').animate({
 				scrollTop: $(hash).offset().top
 			}, 800, function() {
 				console.log('scrolled')
 				window.location.hash = hash;
 			})			
-		
 		} else {
-			
 			event.preventDefault();
-
 			$('body').animate({
 				scrollTop: 0
 			}, 800, function() {
 				window.location.hash = '';
 			})
-
 		}
+	});
 
+
+	$('.form_submit').on('click', 'button', function() {
+
+
+		var name = $('#sender_name').val();
+		var email = $('#sender_email').val();
+		var subject = $('#sender_name').val() + ' via tylercookson.com';
+		var message = $('#sender_message').val();
+		// console.log('clicked');
+
+		$.get('http://localhost:8080/send', {
+			name: name,
+			email: email,
+			subject: subject,
+			message: message
+		}, function(data) {
+			if(data == 'sent') {
+				console.log('sent!!!')
+			} else {
+				console.log('failure')
+			}
+		});
+
+
+		
 	})
 
 
--- server/server.js
+++ server/server.js
@@ -1,14 +1,60 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-var PORT = process.env.PORT || 8080;
+const express = require('express');
+const app = express();
+const path = require('path');
+const nodemailer = require('nodemailer');
+
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname + '/../client')));
 
-
 app.get('/', function (req, res) {
-	res.send('Hello World')
+	res.sendFile('index.html')
 });
+
+
+if(process.env.NODE_ENV !== 'production') {
+	var CONFIG_USERNAME = require('../config/email').CONFIG_USERNAME;
+	var CONFIG_PASSWORD = require('../config/email').CONFIG_PASSWORD;
+	var CONFIG_EMAIL = require('../config/email').CONFIG_EMAIL;
+}
+
+const USERNAME = process.env.USERNAME || CONFIG_USERNAME;
+const PASSWORD = process.env.PASSWORD || CONFIG_PASSWORD;
+const EMAIL = process.env.EMAIL || CONFIG_EMAIL;
+
+
+const smtpTransport = nodemailer.createTransport({
+	service: 'gmail',
+	host: 'smtp.gmail.com',
+	auth: {
+		user: USERNAME,
+		pass: PASSWORD
+	}
+});
+
+app.get('/send', function(req, res) {
+
+	let message = '<div><b>Sender Name: </b>' + req.query.name +'</div><div><b>Sender Email: </b>' + req.query.email + '</div><br /><div>' + req.query.message + '</div>'
+
+	let mailOptions = {
+		to: EMAIL,
+		from: req.query.name,
+		subject: req.query.subject,
+		html: message
+	}
+
+	smtpTransport.sendMail(mailOptions, function(error, response) {
+		if(error) {
+			console.log(error);
+			res.end('error')
+		} else {
+			console.log("message sent");
+			res.end("sent");
+		}
+	});
+});
+
+
 
 app.listen(PORT, function() {
 	console.log("listening on port: " + PORT);
