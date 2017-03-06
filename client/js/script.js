688e9de02c311d7d3c6641ffa961468f2f246ea2
Tyler Cookson <tcookson0805@gmail.com>
2017-03-06 11:11:16 -0800

    update

--- client/js/script.js
+++ client/js/script.js
@@ -5,24 +5,24 @@ $(document).ready(function() {
 
 	// scrolling
 	$('body').on('click', '.nav-link', function(event) {
-		if(this.hash !== ''){
-			event.preventDefault();
-			var hash = this.hash;
-			$('body').animate({
-				scrollTop: $(hash).offset().top
-			}, 800, function() {
-				console.log('scrolled')
-				window.location.hash = hash;
-			})			
-		} else {
-			event.preventDefault();
-			$('body').animate({
-				scrollTop: 0
-			}, 800, function() {
-				window.location.hash = '';
-			})
-		}
+		event.preventDefault();
+		var hash = this.hash;
+		$('body').animate({
+			scrollTop: $(hash).offset().top
+		}, 800, function() {
+			console.log('scrolled')
+			window.location.hash = hash;
+		})			
 	});
+
+	$('body').on('click', '.menu_logo', function(event) {
+		event.preventDefault();
+		$('body').animate({
+			scrollTop: 0
+		}, 800, function() {
+			window.location.hash = '';
+		})
+	})
 
 	// message form
 	$('.form_submit').on('click', 'button', function(event) {
